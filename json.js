795bf81a013c7f78a0ea7971b87209384c2868c8
SkullSyed <syedjack07@gmail.com>
2022-12-27 20:56:24 +0530

    task2

---
+++ json.js
@@ -0,0 +1,17 @@
+function jsonIteration(){
+    for (let i=0; i<input.length;i++){
+        
+    }
+}
+
+const resume = {
+       "FirstName" : "Syed",
+       "SecondName": "Ali",
+       "DOB":"19/04/1996",
+       "Age": "26",
+       "Education": "BA(English)",
+       "Native":"Chennai",
+       "WorkExperience": "5 Years",
+       "MarriedStatus" : "Married"
+
+}
